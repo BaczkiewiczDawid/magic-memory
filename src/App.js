5ecfdcee124b203217a918d6ec4786d031c8dc0f
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-12-23 21:14:07 +0100

    selecting cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import SingleCard from './components/SingleCard/SingleCard';
+import './App.css';
+
+const cardImages = [
+  { "src": "/img/helmet-1.png"},
+  { "src": "/img/potion-1.png"},
+  { "src": "/img/ring-1.png"},
+  { "src": "/img/scroll-1.png"},
+  { "src": "/img/shield-1.png"},
+  { "src": "/img/sword-1.png"},
+]
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - .5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0)
+  }
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log('same');
+      } else {
+        console.log('not same')
+      }
+      resetTurn();
+    }
+  }, [choiceOne, choiceTwo])
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard/SingleCard.js
@@ -0,0 +1,22 @@
+import './SingleCard.css'
+
+const SingleCard = ({ card, handleChoice }) => {
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
+    return ( 
+        <div className="card">
+            <div>
+                <img className="front" src={card.src} alt="card front"></img>
+                <img
+                    className="back"
+                    src="/img/cover.png"
+                    onClick={handleClick}
+                    alt="card back"></img>
+            </div>
+        </div>
+     );
+}
+ 
+export default SingleCard;
